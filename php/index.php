e8e3d9d21c32e9ec15a4bf1e23d7e72b6ce0938b
yuyuyuyuyu1216 <57528209+yuyuyuyuyu1216@users.noreply.github.com>
2021-02-07 17:24:44 +0800

    Update index.php

--- php/index.php
+++ php/index.php
@@ -1,1 +1,23 @@
-
+<?php
+// 存储数据的文件
+$filename = 'github.txt';
+if(!file_exists($filename)) {
+    die($filename . ' 数据文件不存在');
+}
+// 读取整个数据文件
+$data = file_get_contents($filename);
+// 按换行符分割成数组
+$data = explode(PHP_EOL, $data);
+// 随机获取一行索引
+$result = $data[array_rand($data)];
+// 去除多余的换行符（解决获取空值问题
+$result = str_replace(array("\r","\n","\r\n"), '', $result);
+$size_arr = array('large', 'mw1024', 'mw690', 'bmiddle', 'small', 'thumb180', 'thumbnail', 'square');
+$size = !empty($_GET['size']) ? $_GET['size'] : 'large' ;
+$server = rand(1,4);
+if(!in_array($size, $size_arr)){
+	$size = 'large';
+}
+$url = 'https://cdn.jsdelivr.net/gh/yuyuyuyuyu1216/image/image'.$result.'';
+header("Location:".$url);
+?>
